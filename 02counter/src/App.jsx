abbf60ac5825a848543b374a9e81e3a0dfbafeb3
Mrgaurav23 <gauravsinghsisodiya963@gmail.com>
2024-11-06 22:41:17 +0530

    reactfeature config

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   let [counter,setCounter] = useState(0)
-
   let addValue = ()=> {
     if(counter < 100){
       setCounter(counter + 10);
     }
   }
-
   let removeValue = ()=>{
     if(counter > 0){
       setCounter(counter - 10);

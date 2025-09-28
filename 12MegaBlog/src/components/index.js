fc20b47dc4e32092b0795378c13886b6cde2c225
Mrgaurav23 <gauravsinghsisodiya963@gmail.com>
2025-09-28 15:53:38 +0530

    megaBlog commiting

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "../container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE"
+import Button from "../Button"
+import Input from "../Input"
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import Signup from "./Signup"
+import Select from "./Select"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    RTE,
+    PostCard,
+    PostForm,
+    Login,
+    AuthLayout,
+    Signup,
+    Select,
+}
